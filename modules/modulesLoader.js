dc48e417a192e4c7c72bc837364e83de3d1d56d2
illicchpv <illich58@mail.ru>
2024-01-18 20:58:18 +0300

    несколько упростил использование

--- modules/modulesLoader.js
+++ modules/modulesLoader.js
@@ -1,76 +1,76 @@
 var __modulesLoader = (() => {
-  let __loadreadyCalBack = undefined
+  let __loadreadyCalBack = undefined;
 
   function doload(__modulesList, loadreadyCalBack) {
-    __loadreadyCalBack = loadreadyCalBack
-    if(!__modulesList || __modulesList.length == 0){
-      if(__loadreadyCalBack) __loadreadyCalBack()
-      return
+    __loadreadyCalBack = loadreadyCalBack;
+    if (!__modulesList || __modulesList.length == 0) {
+      if (__loadreadyCalBack) __loadreadyCalBack();
+      return;
     }
     {
       __modulesList.forEach(el => {
         // console.log('css module: ', el);
-        if (!el.css) return
+        if (!el.css) return;
         const jsList = el.css.split(';').forEach(css => {
-          css = css.trim()
-          if (css === '') return
+          css = css.trim();
+          if (css === '') return;
           // console.log('css: ', css);
-          const style = document.createElement('link')
-          style.href = css
-          style.setAttribute('rel', 'stylesheet')
-          document.head.append(style)
-        })
-      })
+          const style = document.createElement('link');
+          style.href = css;
+          style.setAttribute('rel', 'stylesheet');
+          document.head.append(style);
+        });
+      });
     }
     {
       for (const el of __modulesList) {
-        el.ok = false
+        el.ok = false;
       }
       for (const el of __modulesList) {
         // console.log('el: ', el);
-        if (!el.js) continue
-        let js = el.js.trim()
-        if (js === '') continue
+        if (!el.js) continue;
+        let js = el.js.trim();
+        if (js === '') continue;
         // console.log('js: ', js);
-        const script = document.createElement('script')
-        script.src = js
-        script.setAttribute('defer', '')
-        document.head.append(script)
-        break
+        const script = document.createElement('script');
+        script.src = js;
+        script.setAttribute('defer', '');
+        document.head.append(script);
+        break;
       }
     }
   }
   function continueLoad(readyEl) {
     // console.log('-------------readyEl: ', readyEl);
-    readyEl.ok = true
+    readyEl.ok = true;
     for (const el of __modulesList) {
       // console.log('el: ', el);
-      if (el.ok) continue
-      if (!el.js) continue
-      let js = el.js.trim()
-      if (js === '') continue
+      if (el.ok) continue;
+      if (!el.js) continue;
+      let js = el.js.trim();
+      if (js === '') continue;
       // console.log('js: ', js);
-      const script = document.createElement('script')
-      script.src = js
-      script.setAttribute('defer', '')
-      document.head.append(script)
-      return
+      const script = document.createElement('script');
+      script.src = js;
+      script.setAttribute('defer', '');
+      document.head.append(script);
+      return;
     }
-    if(__loadreadyCalBack) __loadreadyCalBack()
-    return true
+    if (__loadreadyCalBack) __loadreadyCalBack();
+    return true;
   }
 
-  function getModInst(ModInst){
+  function getModInst(ModInst) {
     // console.log('getModInst(ModInst: ', ModInst, __modulesList);
-    try{
-      const [mod, inst] = ModInst.split('.')
+    try {
+      const [mod, inst] = ModInst.split('.');
       console.log('inst: ', inst);
       for (const el of __modulesList) {
-        if(el.name === mod){
-          return el.module.getInstance(inst)
+        if (el.name === mod) {
+          return el.module.getInstance(inst);
         }
       }
-    }catch(e){
+    } catch (e) {
       console.error('__modulesLoader.ModInst catch(e): ', e);
     }
   }
@@ -78,5 +78,38 @@ var __modulesLoader = (() => {
     continueLoad,
     doload,
     getModInst,
-  }
-})()
+  };
+})();
+
+document.addEventListener("DOMContentLoaded", async function () {
+  __modulesLoader.doload(__modulesList, __modulesReady);
+});
+
+function __modulesReady() {
+  console.log('!!! __modulesReady !!!');
+
+  IncludHtml.doIncludAll(
+    {
+      insertType: "replace", incInner: false,
+      replace: [
+        {from: '../img/', to: './img/'},
+      ],
+      incFromId: "extId",
+    },
+    (defProps) => { // вызывается когда IncludHtml.doIncludAll всё сделал 
+      setTimeout(function () {
+        __modulesList.forEach(el => {el.module.renderAllInstance();});
+      }, 1);
+    }
+  );
+}
+
+function initInstance({module, el, appName, instName}) {
+  const iName = appName + instName;
+  const inst = module.createInstance(iName);
+  if (appName) el.extEl.dataset.app = appName;
+  if (appName) inst.aname = appName;
+  el.extEl.dataset.instance = iName;
+  el.extEl.classList.add(iName);
+  return inst;
+}
